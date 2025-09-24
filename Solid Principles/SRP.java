a34db2eedafcb4f4972181175aa14043c3a6037c
Madhurya kosti <88713046+madhurya1320@users.noreply.github.com>
2025-09-23 23:54:11 -0500

    Add files via upload

---
+++ Solid Principles/SRP.java
@@ -0,0 +1,33 @@
+class DSA {
+    public 
+    void neetcode() {
+        System.out.println( "https://neetcode.io/practice");
+    }
+    void striver() {
+        System.out.println("https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2");
+    }
+}
+
+class LLD {
+    public 
+    void gauravsen() {System.out.println("https://interviewready.io/?srsltid=AfmBOoqQ7fpZ-QBaefEBH28GhiEhKVcrSyVIMzSx-Jfy5vR0PsLIeIan");}
+}
+
+class HLD{
+    public
+    void NC(){System.out.println( "https://neetcode.io/practice");}
+}
+
+
+public class SRP {
+    public static void main(String[] args) {
+        DSA d = new DSA();
+        LLD l = new LLD();
+        HLD h = new HLD();
+
+        d.neetcode();
+        l.gauravsen();
+        h.NC();
+
+    }
+}
